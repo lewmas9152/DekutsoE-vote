f7a7c490bbf03fa404b77a17387baf2dc58e801d
lewmas9152 <smnjuguna22@gmail.com>
2024-01-31 17:14:12 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Homepage from './Homepage'
+
+function App() {
+  
+
+  return (
+    <>
+     <Homepage/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Homepage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Homepage.css'
+
+const Homepage = () => {
+  return (
+    <main className='homepage'>
+      <nav className='nav'>
+        <img src="/assets/logo.svg" alt="logo"  className='logo'/>
+        <ul>
+            <li>Campaigns</li>
+            <li>News</li>
+            <li>Contacts</li>
+        </ul>
+
+        <ul className='navigator'>
+            <li>login <img src="/assets/dropDown.png" alt="dropdown"  className='dropDown'/></li>
+            <li>Signup</li>
+        </ul>
+      </nav>
+
+      <section className='slogan'>
+        <h2>TRASPARENT ,SECURE AND CREDIBLE ONLINE BASED SCHOOL ELECTIONS</h2>
+        <p>Revolutionalising the voting experience by creating a safe and more secure space for carring out school elections.</p>
+      </section>
+
+      <div className='footer'>
+      <p>&copy; 2023-2024 DEKUTSO E-VOTE, Inc. All Rights Reserved</p>
+      </div>
+    </main>
+  )
+}
+
+export default Homepage
