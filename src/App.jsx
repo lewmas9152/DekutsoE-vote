9ed9bd8c3f49f224b489387f5b7599130cf97b4e
lewmas9152 <smnjuguna22@gmail.com>
2024-02-27 17:33:34 +0300

    React router refresh problem fixed. Basename added to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -395,7 +395,7 @@ function App() {
               </div>
             </nav>
 
-            <Routes>
+            <Routes basename ="/DekutsoE-vote/">
               <Route path="/" element={<Homepage />} />
               <Route path="/CreateCandidate" element={<CreateCandidate />} />
               <Route path="/dashboard" element={<Dashboard />} />
